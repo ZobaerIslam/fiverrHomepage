ee75ad2ca091ddc3d20aaa87b6478cf8e712f5e7
ZobaerIslam <zobaerislam62@gmail.com>
2022-01-19 06:24:41 +0600

    built the fiverr made on slider section

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ var flkty = new Flickity( elem, {
   contain: true,
   wrapAround: true
 });
+
+var elem = document.querySelector('.fiverr__madeon__slider__sample');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true,
+});
